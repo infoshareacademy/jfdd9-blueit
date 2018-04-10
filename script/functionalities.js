29023119d7e9d19ad9a9da18e80decb3f1b839ba
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-04-10 22:08:50 +0200

    FadeIn effect first try

--- script/functionalities.js
+++ script/functionalities.js
@@ -5,11 +5,24 @@ function funcBoxesClassSwitch () {
 
 document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener("scroll", function () {
-        var func1 = document.getElementById("func-1");
-        if (document.documentElement.scrollTop > 450) {
-            func1.classList.add("func-show");
-        } else {
-            func1.classList.remove("func-show");
+        var box1 = document.querySelector(".func-box-1");
+        var box2 = document.querySelector(".func-box-2");
+        if (document.documentElement.scrollTop > 400) {
+            if (box1.classList.contains('opacity0') && box2.classList.contains('opacity0')){
+                box1.classList.remove('opacity0');
+                box1.classList.add('opacity1');
+                box2.classList.remove('opacity0');
+                box2.classList.add('opacity1');
+            }
         }
+        else {
+            // el.classList.add('is-paused');
+            box1.classList.remove('opacity1');
+            box1.classList.add('opacity0');
+            box2.classList.remove('opacity1');
+            box2.classList.add('opacity0');
+        }
+
+
     });
 });
