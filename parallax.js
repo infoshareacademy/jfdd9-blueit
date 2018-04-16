ed71b68c597e04d3c870ac168a4aab571c3d16e3
agatapier <agata.piernikowska@yahoo.com>
2018-04-16 16:03:02 +0200

    zmiana polozenia parallaxy

--- parallax.js
+++ parallax.js
@@ -1,6 +1,6 @@
 
 window.addEventListener('scroll', function (event) {
-    document.querySelector('.hero-headline').style.top = (window.pageYOffset * 1.1) +'px';
+    document.querySelector('.hero-headline').style.top = (100+ window.pageYOffset * 1.1) +'px';
 
 })
 
