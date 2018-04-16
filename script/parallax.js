5c25941d1b8fe02a474371d77d0d1d6027cbdd31
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-04-16 22:27:53 +0200

    reformat code + wymiana zdjecia Robert

--- script/parallax.js
+++ script/parallax.js
@@ -1,6 +1,5 @@
-
 window.addEventListener('scroll', function (event) {
-    document.querySelector('.hero-headline').style.top = (100+ window.pageYOffset * 1.5) +'px';
+    document.querySelector('.hero-headline').style.top = (100 + window.pageYOffset * 1.5) + 'px';
 
 })
 
