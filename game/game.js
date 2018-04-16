ae587eb16685c52834f3d53a9cbf9ab76f7e897e
lkoziara <lukasz.koziara88@gmail.com>
2018-04-16 16:13:46 +0200

    road mooooviiiinnn' smoooothly

--- game/game.js
+++ game/game.js
@@ -4,16 +4,17 @@ var game = (function () {
     var direction = 0;
 
     function moveRight() {
-        direction = 1;
+        direction = 3;
     }
+
     function moveLeft() {
-        direction = -1;
+        direction = -3;
     }
 
     function enableControls() {
         document.addEventListener('keydown', function (event) {
             console.log(event.code);
-            switch(event.code) {
+            switch (event.code) {
                 case 'ArrowRight':
                     moveRight();
                     break;
@@ -23,22 +24,36 @@ var game = (function () {
             }
         })
     }
+
+    var y = 0;
+    requestAnimationFrame(move);
+    var road = document.getElementById('road');
+
+    function move() {
+        y += 3;
+        road.style.backgroundPosition = '0 ' + y + 'px';
+        requestAnimationFrame(move);
+
+    }
+
     function getCar() {
         return document.querySelector('.car')
     }
-    function start () {
+
+    function start() {
         enableControls()
 
         setInterval(function () {
             var marginLeft = Math.max(
                 Math.min(
-                parseInt(getCar().style.marginLeft || 0),
-                501
-            ),0);
+                    parseInt(getCar().style.marginLeft || 0),
+                    501
+                ), 0);
             getCar().style.marginLeft = (marginLeft + direction) + 'px'
         }, 16);
 
     }
+
     return {
         start: start
     }
