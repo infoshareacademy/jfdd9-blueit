1e7c29e13edc720eb18b48b20161ebd270985835
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-04-19 22:44:43 +0200

    zmiana parallax, zmiana hero headline 'top' i @media

--- script/parallax.js
+++ script/parallax.js
@@ -1,5 +1,7 @@
 window.addEventListener('scroll', function (event) {
-    document.querySelector('.hero-headline').style.top = (100 + window.pageYOffset * 0.85) + 'px';
-
-})
+    var heroHeadline = document.querySelector('.hero-headline');
+    $(window).width() < 992
+        ? heroHeadline.style.top = (100 + window.pageYOffset * 0.7) + 'px'
+        : heroHeadline.style.top = (100 + window.pageYOffset * 0.77) + 'px';
+});
 
