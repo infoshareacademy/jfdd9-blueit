d7a9bf1a49b61a508933d02cebc778e1a03deadd
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-04-19 21:01:33 +0200

    zmiana zdjecia lukasz, zmiana parallaxy

--- script/parallax.js
+++ script/parallax.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function (event) {
-    document.querySelector('.hero-headline').style.top = (100 + window.pageYOffset * 1.5) + 'px';
+    document.querySelector('.hero-headline').style.top = (100 + window.pageYOffset * 0.85) + 'px';
 
 })
 
