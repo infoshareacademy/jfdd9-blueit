35e0b6ee7c01bc13ef45086475c204fece13329d
lkoziara <lukasz.koziara88@gmail.com>
2018-04-20 15:32:52 +0200

    car stops at keyup, XY movment, class change

--- game/game.js
+++ game/game.js
@@ -105,13 +105,15 @@ theme.play()
         }, 30);
     }
 
-
     var score = 0;
     var timer = 0;
     var scoreIntervalId;
     var timerIntervalId;
     var pointsForBattery = 5;
     var pointsPerSecond = 2;
+
+    var progress = document.querySelector('.progress')
+
 
     function getLastScreen() {
         return document.querySelector('.last-screen');
