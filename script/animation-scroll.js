f8b6f9af190a2ab61c6a27dc968076de9deec817
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-05-28 17:00:44 +0200

    WIP: Highlighting sections on scroll

--- script/animation-scroll.js
+++ script/animation-scroll.js
@@ -1,5 +1,6 @@
 var links = document.querySelectorAll('#hero-section a[href^="#"]');
-var menu = document.querySelector('#hero-section');
+var navLinks = document.querySelectorAll('#nav-div a[href^="#"]');
+var menu = document.querySelector('#hero-nav');
 
 links.forEach(function (link) {
   link.addEventListener('click', function (event) {
@@ -12,23 +13,24 @@ links.forEach(function (link) {
     });
   });
 });
-//
-// window.addEventListener('scroll', function (event) {
-//   var currentPosition = window.pageYOffset;
-//   Array.prototype.map.call(links, function (link) {
-//     return document.querySelector(
-//       link.getAttribute('href')
-//     )
-//   }).forEach(function (element, index) {
-//     var pos = currentPosition + menu.offsetHeight + 1;
-//     if (
-//       pos > element.offsetTop &&
-//       pos < element.offsetHeight + element.offsetTop) {
-//       links.forEach(function (link) {
-//         link.classList.remove('active')
-//       });
-//       links[index].classList.add('active')
-//     }
-//   })
-// });
+
+window.addEventListener('scroll', function (event) {
+  var currentPosition = window.pageYOffset;
+  Array.prototype.map.call(navLinks, function (link) {
+    return document.querySelector(
+      link.getAttribute('href')
+    )
+  }).forEach(function (element, index) {
+    var pos = currentPosition + menu.offsetHeight + 1;
+    if (
+      pos > element.offsetTop &&
+      pos < element.offsetHeight + element.offsetTop
+    ) {
+      navLinks.forEach(function (link) {
+        link.classList.remove('active')
+      });
+      navLinks[index].classList.add('active')
+    }
+  })
+});
 
